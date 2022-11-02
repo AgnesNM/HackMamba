cff4351b64b9d620a92616108410f7ac0a23075f
AgnesNM <agnesndutamwaniki@gmail.com>
2022-11-02 20:05:19 +0300

    add code to store response data in a json file

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-require('dotenv').config()
-const apiKey = process.env.XATA_API_KEY;
+const fs = require("fs");
 
+require('dotenv').config();
+const apiKey = process.env.XATA_API_KEY;
+    
 const options = {
-    method: 'POST',
-    headers: {Authorization: `Bearer ${apiKey}`, 'Content-Type': 'application/json'},
-    body: '{"page":{"size":15}}'
-  };
-  
-  fetch('https://agnesnduta-mqqqgp.us-east-1.xata.sh/db/HackMambaTest:main/tables/Users/query', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+  method: 'GET',
+  headers: {Authorization: `Bearer ${apiKey}`, 'Content-Type': 'application/json'}  
+};
+      
+fetch('https://agnesnduta-mqqqgp.us-east-1.xata.sh/db/HackMambaTest:main/tables/Users/data/rec_cdf02f39dsj678levd8g', options)
+.then((response) => { 
+  return response.json();
+})
+.then((json) => {
+  fs.writeFile('./users.json', JSON.stringify(json), (err) => {
+    if (err) {
+      throw new Error('Something went wrong.')
+    }
+    console.log('JSON written to file. Contents:');
+    console.log(fs.readFileSync('users.json', 'utf-8'))
+  })
+});
