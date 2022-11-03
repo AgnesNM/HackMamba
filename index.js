0c9303b7d7d9e0175ff1b027177760cc4a68a5c4
AgnesNM <agnesndutamwaniki@gmail.com>
2022-11-03 12:18:07 +0300

    send the image URL to Xata

--- index.js
+++ index.js
@@ -48,3 +48,13 @@ fetch('https://agnesnduta-mqqqgp.us-east-1.xata.sh/db/HackMambaTest:main/tables/
 
 // // route to update a specific user's profile pic 
 
+const addProfilePic = {
+  method: 'PATCH',
+  headers: {Authorization: `Bearer ${apiKey}`, 'Content-Type': 'application/json'},
+  body: '{"first_name":"Ricky","last_name":"Port","period_covered":"120","cover_status":true,"profile_pic":"http://res.cloudinary.com/ndutared/image/upload/v1667460232/nr3uwls3q6ew8co39thn.jpg"}'
+};
+
+fetch('https://agnesnduta-mqqqgp.us-east-1.xata.sh/db/HackMambaTest:main/tables/Users/data/rec_cdf02f39dsj678levd8g', addProfilePic)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
