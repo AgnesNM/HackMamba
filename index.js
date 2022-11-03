fb22f1e530699dd7b918f2b03132a10d7a046379
AgnesNM <agnesndutamwaniki@gmail.com>
2022-11-03 12:07:52 +0300

    uploading the user’s profile picture to Cloudinary

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const fs = require("fs");
+require("dotenv").config();
+let cloudinary = require('cloudinary').v2;
 
-require('dotenv').config();
 const apiKey = process.env.XATA_API_KEY;
+
+// cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret
+});
+
+// upload our image to Cloudinary 
+
+cloudinary.uploader.upload("water.jpg", 
+  {resource_type: "image" })
+.then((result) => {
+  fs.writeFile('./images.json', JSON.stringify(result), (err) => {
+    if (err) {
+      throw new Error('Oops, something went wrong.')
+    }
+    console.log(result.url);
+    
+  })
+});
+
+
+// route to retrieve a specific user using a specific user ID
     
 const options = {
   method: 'GET',
@@ -17,7 +42,9 @@ fetch('https://agnesnduta-mqqqgp.us-east-1.xata.sh/db/HackMambaTest:main/tables/
     if (err) {
       throw new Error('Something went wrong.')
     }
-    console.log('JSON written to file. Contents:');
-    console.log(fs.readFileSync('users.json', 'utf-8'))
+    console.log(json);
   })
 });
+
+// // route to update a specific user's profile pic 
+
